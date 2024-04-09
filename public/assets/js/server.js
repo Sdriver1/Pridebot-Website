254c35779cd50635a2259f2cd55e2a813e2684e3
Sdriver1 <sjdriver08@gmail.com>
2024-04-08 21:14:32 -0400

    Website changes

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-import fetch from "node-fetch";
 
 const app = express();
 const port = 3000;
@@ -12,33 +11,22 @@ app.use(express.json());
 app.get("/", (req, res) => res.sendFile("index.html", { root: __dirname }));
 app.get("/tos", (req, res) => res.sendFile("tos.html", { root: __dirname }));
 app.get("/privacy", (req, res) =>
-  res.sendFile("privacy.html", { root: __dirname }),
+  res.sendFile("privacy.html", { root: __dirname })
 );
 app.get("/morbius", (req, res) =>
-  res.sendFile("morbius.html", { root: __dirname }),
+  res.sendFile("morbius.html", { root: __dirname })
 );
 app.get("/invite", (req, res) =>
-  res.sendFile("invite.html", { root: __dirname }),
+  res.sendFile("invite.html", { root: __dirname })
 );
 app.get("/partners", (req, res) =>
-  res.sendFile("partners.html", { root: __dirname }),
+  res.sendFile("partners.html", { root: __dirname })
 );
 
 app.listen(port, () => {
   console.log(`API is running on port ${port}`);
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/wumpus", (req, res) => {
-  let userid = req.body.userId;
-  let botid = req.body.botId;
-
-  //Add whatever you want to do when there was a vote here
-  console.log(userid + " just voted for " + botid);
-});
-
 app.listen(port, () =>
-  console.log(`Server running at http://localhost:${port}`),
+  console.log(`Server running at http://localhost:${port}`)
 );
--- public/assets/js/untitled.js
+++ public/assets/js/untitled.js
@@ -1,6 +1,4 @@
 $(function() {
-  // This will select everything with the class smoothScroll
-  // This should prevent problems with carousel, scrollspy, etc...
   $('.smoothScroll').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -8,7 +6,7 @@ $(function() {
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top
-        }, 1000); // The number here represents the speed of the scroll in milliseconds
+        }, 1000); 
         return false;
       }
     }
