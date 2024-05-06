cb8f49cff76d545231ed83f4c9b8ac151617a019
Sdriver1 <sjdriver08@gmail.com>
2024-05-06 07:30:33 -0400

    Added /profiles (still under work

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,35 +1,36 @@
 import express from "express";
-import bodyParser from "body-parser";
+import path from "path";
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname));
-app.use(bodyParser.json());
+app.use(express.static(path.resolve(__dirname)));
 app.use(express.json());
 
-app.get("/", (req, res) => res.sendFile("index.html", { root: __dirname }));
-app.get("/tos", (req, res) => res.sendFile("tos.html", { root: __dirname }));
+app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "index.html")));
+app.get("/tos", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "tos.html"))
+);
 app.get("/privacy", (req, res) =>
-  res.sendFile("privacy.html", { root: __dirname })
+  res.sendFile(path.resolve(__dirname, "privacy.html"))
 );
 app.get("/morbius", (req, res) =>
-  res.sendFile("morbius.html", { root: __dirname })
+  res.sendFile(path.resolve(__dirname, "morbius.html"))
 );
 app.get("/invite", (req, res) =>
-  res.sendFile("invite.html", { root: __dirname })
+  res.sendFile(path.resolve(__dirname, "invite.html"))
 );
 app.get("/support", (req, res) =>
-  res.sendFile("support.html", { root: __dirname })
+  res.sendFile(path.resolve(__dirname, "support.html"))
 );
 app.get("/partners", (req, res) =>
-  res.sendFile("partners.html", { root: __dirname })
+  res.sendFile(path.resolve(__dirname, "partners.html"))
+);
+
+app.get("/profiles/:userId", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "profiles.html"))
 );
 
 app.listen(port, () => {
-  console.log(`API is running on port ${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
-
-app.listen(port, () =>
-  console.log(`Server running at http://localhost:${port}`)
-);
