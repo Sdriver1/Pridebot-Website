657a028e2a13fbb8431aa04c20c6c58b97ecf832
Sdriver1 <sjdriver08@gmail.com>
2024-08-12 15:24:04 -0400

    Site changes

--- public/assets/js/server.mjs
+++ public/assets/js/server.mjs
@@ -5,7 +5,7 @@ import fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const port = 3000;
+const port = 8081;
 const app = express();
 
 const serveStatic = (route, directory) => {
